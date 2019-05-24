2b96db04155d5db62ef821350e49b8f1cd86f62f
Ferdysan <ferdysan87@gmail.com>
2019-05-24 14:43:31 +0200

    inizio

--- main.js
+++ main.js
@@ -3,53 +3,78 @@
 // Ogni volta che cambio mese dovrò: 1. Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018) 2. Controllare quanti giorni ha il mese scelto formando così una lista
 
 $(document).ready(function(){
+
+
+  var template_html = $('#template-calendario').html();
+  var template_function = Handlebars.compile(template_html);
+
   // tramite moment js inserisco una variabile con il formato data adatto
+  var min_date='2018-01-01';
+  var max_date= '2018-12-01';
 
-
- $('#mese-successivo').click(function(){
-
-    var date='2018-01-01';
-
-
+    var inizial_date='2018-01-01';
     // in un altra varaibile passo al metodo moment la variabile date
+    var current_date = moment(inizial_date);
+    disegna_mese(current_date);
 
-    var moment_date = moment(date);
-    //posso fare un log per provare
-    console.log(moment_date);
+   // intercetto il click sul mese successivo
+    $('#mese-successivo').click(function(){
+
+      if (current_date.isSameOrAfter(max_date)) {
+        alert('data oltre il limite max');
+        $(this).attr('disable', true);
+      }else{
+        current_date.add(1 , 'months');
+        disegna_mese(current_date);
+        $(this).attr('disable', false);
+      }
+    });
+
+    $('#mese-precedente').click(function(){
+
+      if (current_date.isSameOrBefore(min_date)) {
+        alert('data oltre il limite min');
+        $(this).attr('disable', true);
+      }else{
+        current_date.subtract(1 , 'months');
+        disegna_mese(current_date);
+      }
+    });
 
 
-    //nella libreria moment.js trovo un metodo per trovarmi quanti giorni ha un mese
-    var giorni = moment_date.daysInMonth();
+  //  funzione che mi disegna il mese
+    function disegna_mese(moment_date){
+      // resetto il contenitore
+      $('#calendario').empty();
+      // leggo quanti giorni ci sono nel mese corrente
+      var giorni = moment_date.daysInMonth();
 
-    var mese = moment_date.format('MMMM');
+       // preparo le variabili da utilizzare nel display del mese
+      var mese = moment_date.format('MMMM');
+      var anno = moment_date.format('YYYY');
 
-    //scorro la variabile giorni che contiene un numero
-    //parto da 1
+      // inserisoc nel titolo il mese corrente
+      $('#mese_corrente').text(mese + ' ' + anno);
 
-    for (var i = 1; i <= giorni; i++) {
-      $('#calendario').append('<li>' + i + ' ' + mese + '</li>');
+      //scorro la variabile giorni che contiene un numero
+      //parto da 1
+      for (var i = 1; i <= giorni; i++) {
+        var giorno = i + ' ' + mese;
+        var variables ={
+          'giorno_template':giorno
+          'data-giorno_iso':moment_date.format('YYYY-MM-')+ format_day(i);
+        }
+        $('#calendario').append(template_function(variables));
+      }
     }
- });
 
- $('#mese-precedente').click(function(){
-   // tramite moment js inserisco una variabile con il formato data adatto
-    var date= '2018-01-01';
-    // in un altra varaibile passo al metodo moment la variabile date
-    var moment_date = moment(date);
-    //posso fare un log per provare
-
-
-    //nella libreria moment.js trovo un metodo per trovarmi quanti giorni ha un mese
-    var giorni = moment_date.daysInMonth();
-
-    var mese = moment_date.format('MMMM');
-
-    //scorro la variabile giorni che contiene un numero
-    //parto da 1
-
-    for (var i = 1; i <= giorni; i++) {
-      $('#calendario').append('<li>' + i + ' ' + mese + '</li>');
+   // funzione che mi aggiunge lo 0 davanti a numeri singoli
+    function format_day(day){
+      if(day<10){
+        return '0' + day
+      }
+      return day;
     }
- });
 
-});
+
+  });
